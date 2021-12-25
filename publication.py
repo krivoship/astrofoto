e7855043ee70c3d8290f2651b161388ce07d1a75
Mikhail Beshkarev <krivoship@bk.ru>
2021-12-26 01:08:14 +0300

    publish photo with time.sleep()

--- publication.py
+++ publication.py
@@ -1,15 +1,24 @@
 import telegram
 import os
 from random import choice
+import time
 from dotenv import load_dotenv
 
-load_dotenv()
-tg_token = os.getenv("TELEGRAM_TOKEN")
-chat_id = os.getenv("CHAT_ID")
 
-images = os.listdir('./images')
-random_image = choice(images)
-image_path = os.path.join('./images', random_image)
+def main():
+    load_dotenv()
+    tg_token = os.getenv("TELEGRAM_TOKEN")
+    chat_id = os.getenv("CHAT_ID")
+    time_step = int(os.getenv("TIME_STEP", 24*60*60))
 
-bot = telegram.Bot(token=tg_token)
-bot.send_photo(chat_id=chat_id, photo=open(image_path, 'rb'))
+    images = os.listdir('./images')
+    bot = telegram.Bot(token=tg_token)
+
+    while True:
+        random_image = choice(images)
+        image_path = os.path.join('./images', random_image)
+        bot.send_photo(chat_id=chat_id, photo=open(image_path, 'rb'))
+        time.sleep(time_step)
+
+if __name__ == '__main__':
+    main()
