8749c5a6d1c5b6ffeace53e2ab9b5ae80a53133d
Mikhail Beshkarev <krivoship@bk.ru>
2021-12-26 00:48:35 +0300

    publish photo to Telegram channel

--- publication.py
+++ publication.py
@@ -1,10 +1,15 @@
 import telegram
 import os
+from random import choice
 from dotenv import load_dotenv
 
 load_dotenv()
 tg_token = os.getenv("TELEGRAM_TOKEN")
 chat_id = os.getenv("CHAT_ID")
 
+images = os.listdir('./images')
+random_image = choice(images)
+image_path = os.path.join('./images', random_image)
+
 bot = telegram.Bot(token=tg_token)
-bot.send_message(chat_id=chat_id, text="Test")
+bot.send_photo(chat_id=chat_id, photo=open(image_path, 'rb'))
